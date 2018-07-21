9d20d3a5e6b7422d6b3fa73a8b216aa00fb5b247
Carlos Adrián Martínez <carlos_adrian_m@hotmail.com>
2018-07-21 12:29:51 +0200

    Primer version

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,18 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 
+import * as firebase from 'firebase';
+
+var config = {
+  apiKey: "AIzaSyD9rhKPG-xt3kiR_kZQ0FEK_AiPpeIM8rc",
+  authDomain: "vendedor-bbf2b.firebaseapp.com",
+  databaseURL: "https://vendedor-bbf2b.firebaseio.com",
+  projectId: "vendedor-bbf2b",
+  storageBucket: "vendedor-bbf2b.appspot.com",
+  messagingSenderId: "356423060798"
+};
+firebase.initializeApp(config);
+
 @NgModule({
   declarations: [
     MyApp,
