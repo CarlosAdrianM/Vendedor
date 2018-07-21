c8786ef0668d1ebc37fa9e4a261fd52e173c114d
Carlos Adrián Martínez <carlos_adrian_m@hotmail.com>
2018-07-22 00:49:59 +0200

    Primer commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -9,7 +9,7 @@ import 'firebase/firestore';
 })
 export class HomePage {
 
-  messages: any;
+  clientes: any;
   private db: any;
   model: any = {};
   isEditing: boolean = false;
@@ -19,110 +19,111 @@ export class HomePage {
     this.loadData();
   }
 
-  loadData(){
-    this.getAllDocuments("messages").then((e)=>{
-      this.messages = e;
-  });
-  }
- 
-addMessage(){
-    if(!this.isEditing){
-    this.addDocument("messages", this.model).then(()=>{
-      this.loadData();//refresh view
+    loadData(){
+        this.getAllDocuments("clientes").then((e)=>{
+            this.clientes = e;
+        });
+    }
+        
+    addMessage(){
+        if(!this.isEditing){
+            this.addDocument("clientes", this.model).then(()=>{
+                this.loadData();//refresh view
+            });
+        }else{
+            this.updateDocument("clientes", this.model.$key, this.model).then(()=>{
+                this.loadData();//refresh view
+            });
+        }
+        this.isEditing = false;
+        //clear form
+        this.model.nombre = '';
+        this.model.direccion = '';
+        this.model.email = '';
+    }
+    
+    updateMessage(obj){
+        this.model = obj;
+        this.isEditing = true;
+    }
+    
+    deleteMessage(key){
+    this.deleteDocument("clientes", key).then(()=>{
+        this.loadData();//refresh view
+        this.isEditing = false;
     });
-  }else{
-    this.updateDocument("messages", this.model.$key, this.model).then(()=>{
-      this.loadData();//refresh view
-    });
-  }
-  this.isEditing = false;
-  //clear form
-  this.model.nombre = '';
-  this.model.direccion = '';
-  this.model.email = '';
-}
- 
-updateMessage(obj){
-  this.model = obj;
-  this.isEditing = true;
-}
- 
-deleteMessage(key){
-  this.deleteDocument("messages", key).then(()=>{
-    this.loadData();//refresh view
-    this.isEditing = false;
-  });
-}
-
-getAllDocuments(collection: string): Promise<any> {
-  return new Promise((resolve, reject) => {
-      this.db.collection(collection)
-          .get()
-          .then((querySnapshot) => {
-              let arr = [];
-              querySnapshot.forEach(function (doc) {
-                  var obj = JSON.parse(JSON.stringify(doc.data()));
-                  obj.$key = doc.id
-                  console.log(obj)
-                  arr.push(obj);
-              });
-
-              if (arr.length > 0) {
-                  console.log("Document data:", arr);
-                  resolve(arr);
-              } else {
-                  console.log("No such document!");
-                  resolve(null);
-              }
+    }
 
 
-          })
-          .catch((error: any) => {
-              reject(error);
-          });
-  });
-}
 
-deleteDocument(collectionName: string, docID: string): Promise<any> {
-return new Promise((resolve, reject) => {
-    this.db
-        .collection(collectionName)
-        .doc(docID)
-        .delete()
-        .then((obj: any) => {
-            resolve(obj);
-        })
-        .catch((error: any) => {
-            reject(error);
+
+    // Sacar a un documento común-----------------------------------------------
+    getAllDocuments(collection: string): Promise<any> {
+    return new Promise((resolve, reject) => {
+        this.db.collection(collection)
+            .get()
+            .then((querySnapshot) => {
+                let arr = [];
+                querySnapshot.forEach(function (doc) {
+                    var obj = JSON.parse(JSON.stringify(doc.data()));
+                    obj.$key = doc.id
+                    console.log(obj)
+                    arr.push(obj);
+                });
+
+                if (arr.length > 0) {
+                    console.log("Document data:", arr);
+                    resolve(arr);
+                } else {
+                    console.log("No such document!");
+                    resolve(null);
+                }
+            })
+            .catch((error: any) => {
+                reject(error);
+            });
         });
-});
-}
+    }
 
-addDocument(collectionName: string, dataObj: any): Promise<any> {
-return new Promise((resolve, reject) => {
-    this.db.collection(collectionName).add(dataObj)
-        .then((obj: any) => {
-            resolve(obj);
-        })
-        .catch((error: any) => {
-            reject(error);
+    deleteDocument(collectionName: string, docID: string): Promise<any> {
+    return new Promise((resolve, reject) => {
+        this.db
+            .collection(collectionName)
+            .doc(docID)
+            .delete()
+            .then((obj: any) => {
+                resolve(obj);
+            })
+            .catch((error: any) => {
+                reject(error);
+            });
         });
-});
-}
+    }
 
-updateDocument(collectionName: string, docID: string, dataObj: any): Promise<any> {
-return new Promise((resolve, reject) => {
-    this.db
-        .collection(collectionName)
-        .doc(docID)
-        .update(dataObj)
-        .then((obj: any) => {
-            resolve(obj);
-        })
-        .catch((error: any) => {
-            reject(error);
+    addDocument(collectionName: string, dataObj: any): Promise<any> {
+    return new Promise((resolve, reject) => {
+        this.db.collection(collectionName).add(dataObj)
+            .then((obj: any) => {
+                resolve(obj);
+            })
+            .catch((error: any) => {
+                reject(error);
+            });
         });
-});
-}
+    }
 
+    updateDocument(collectionName: string, docID: string, dataObj: any): Promise<any> {
+        return new Promise((resolve, reject) => {
+            this.db
+                .collection(collectionName)
+                .doc(docID)
+                .update(dataObj)
+                .then((obj: any) => {
+                    resolve(obj);
+                })
+                .catch((error: any) => {
+                    reject(error);
+                });
+        });
+    }
 }
